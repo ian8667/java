ed87450d63a67e1c0a3b84131dd7625dc837c118
Ian <ian3924@yahoo.co.uk>
2019-05-24 21:40:14 +0100

    Final code changes
    Final code changes to the program.

--- Compress_Gzip.java
+++ Compress_Gzip.java
@@ -6,11 +6,13 @@ import java.io.FileOutputStream;
 import java.util.zip.GZIPOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.BufferedInputStream;
+import java.io.File;
 /**
  * Compresses a file using the GZIP file format.
  *
  * @author Ian Molloy May 2019
- * @version (#)Compress_Gzip.java        1.00 2019-05-22
+ * @version (#)Compress_Gzip.java        1.01 2019-05-24
  */
 public class Compress_Gzip {
 private byte dummy;
@@ -23,40 +25,47 @@ private byte dummy;
   }//end of constructor
 
   /**
-   * Working test method.
-   * Floating point formatting to decimal places: %.2f
-   * A format of %03d will pad, for example, a 5 to 005.
-   * topLevel.setLocationRelativeTo(null);
+   * Working method.
    */
   public void launchFrame() {
-    System.out.printf("Start of test on %tc%n", new java.util.Date());
+    System.out.printf("Start of compress on %tc%n", new java.util.Date());
     // ---------------------------------------------------------------
-    String inputFile = "C:\\Test\\small_sampledata.txt";
+    String inputFile = "C:\\Test\\very_smalldata.txt";
     String outputFile = "C:\\Test\\gashoutput.gz";
-byte[] databuffer = new byte[1024 * 4];
 
-int bytesread = 0;
+    long bytestransferred = 0L;
+    long filelength = (new File(inputFile)).length();
+    long difference = 0L;
 
-try (FileInputStream fis = new FileInputStream(inputFile);
-     FileOutputStream fos = new FileOutputStream(outputFile);
-     GZIPOutputStream gos = new GZIPOutputStream(fos);)
-{
+    try (// Input objects
+         FileInputStream fis = new FileInputStream(inputFile);
+         BufferedInputStream buffread = new BufferedInputStream(fis);
+         // Output objects
+         FileOutputStream fos = new FileOutputStream(outputFile);
+         GZIPOutputStream gos = new GZIPOutputStream(fos);)
+    {
 
-while ((bytesread = fis.read(databuffer)) != -1) {
-       gos.write(databuffer, 0, bytesread);
-}
+       bytestransferred = buffread.transferTo(gos);
 
-} catch (FileNotFoundException e) {
-  System.err.println("File not found problem");
-  e.printStackTrace();
-} catch (IOException e2) {
-  System.err.println("IO problem of some kind");
-  e2.printStackTrace();
+    } catch (FileNotFoundException e) {
+      System.err.println("File not found problem");
+      e.printStackTrace();
+    } catch (IOException e2) {
+      System.err.println("IO problem of some kind");
+      e2.printStackTrace();
+    } //end try/catch block
 
-}
+
+    System.out.println("\nFiles used");
+    System.out.printf("Input file: %s%nOutput file: %s%n", inputFile, outputFile);
+
+    difference = Math.abs(filelength - bytestransferred);
+    System.out.printf("\nInput file length: %d bytes%n", filelength);
+    System.out.printf("Bytes transferred to Gzip: %d bytes%n%n", bytestransferred);
+    System.out.printf("Difference: %d bytes%n", difference);
 
     // ---------------------------------------------------------------
-    System.out.printf("End of test on %tc%n", new java.util.Date());
+    System.out.printf("End of compress on %tc%n", new java.util.Date());
   }//end of launchFrame
 
   /**
