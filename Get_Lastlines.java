b773b6c0a1d1d025b65af6a4beaa0372e76f04b3
Ian <ian3924@yahoo.co.uk>
2020-07-20 10:58:51 +0100

    Corrected the use of method 'transferTo'
    Corrected the use of method FileChannel.transferTo.

--- Get_Lastlines.java
+++ Get_Lastlines.java
@@ -3,17 +3,20 @@
 import java.nio.channels.FileChannel;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 /**
  * Obtains the last N bytes of a file writing those bytes to a
  * separate file. The number of bytes (N) to obtain is determined
  * by the user.
  *
+ * Java SE API docs
+ * https://docs.oracle.com/en/java/javase/14/docs/api/index.html
+ *
  * @author Ian Molloy June 2019
- * @version (#)Get_Lastlines.java        1.00 2019-06-05
+ * @version (#)Get_Lastlines.java        1.01 2020-07-18T13:47:38
  */
 public class Get_Lastlines {
 private byte dummy;
-private long ChunkSize;
   /**
    * Constructor
    */
@@ -29,36 +32,56 @@ private long ChunkSize;
     System.out.printf("Start of run on %tc%n", new java.util.Date());
     // ---------------------------------------------------------------
    System.out.println("Hello world");
-   final int kb1 = 1024; // One Kilobyte
-   final int mb1 = 1048576; // One Megabyte
+   final int kb1 = 1024;     // One Kilobyte
+   final int mb1 = 1048576;  // One Megabyte
 
-   final String inputfile = "C:\\test\\small_sampledata.txt";
+   final String inputfile  = "C:\\test\\small_sampledata.txt";
    final String outputfile = "C:\\test\\gashoutput.txt";
-   // Number of bytes to obtain from end of file.
-   final int lastbytes = (kb1 * 5);
-
+   // Number of wanted bytes to obtain from end of file.
+   final int wantedBytes = (kb1 * 1);
    final long fileLength = (new File(inputfile).length());
 
-   final long filepointer = (fileLength - lastbytes);
+   //As the number of wanted bytes may be greater than the
+   //file length, we make the assumption the whole file
+   //is to be copied to the destination file. So this
+   //means the file position will start from file position
+   //zero (the beginning of the file and hence why a
+   //file position of zero).
+   long filePos = 0L;
+
+   // Set the file position (pointer) to the required postion
+   // in the file to give us the required number bytes to the
+   // end of the file. In other words, we skip over the bytes
+   // at the start of the file we're not interested in.
+   if (wantedBytes < fileLength) {
+      filePos = fileLength - wantedBytes;
+   }
+
    long bytestransferred = 0L;
+
    System.out.println("");
    System.out.printf("Input file length is %d bytes%n", fileLength);
-   System.out.printf("Number of bytes to obtain: %d%n", lastbytes);
+   System.out.printf("Number of bytes to transfer to output file: %d%n", wantedBytes);
+
 
    try (FileChannel inChan = (new FileInputStream(inputfile)).getChannel();
-        FileChannel outChan = (new FileInputStream(outputfile)).getChannel(); )
+        FileChannel outChan = (new FileOutputStream(outputfile)).getChannel(); )
    {
 
-    bytestransferred = inChan.transferTo(filepointer,
-                                         (long)lastbytes,
-                                         outChan);
+    //Returns: The number of bytes, possibly zero, that were
+    //actually transferred
+    bytestransferred = inChan.transferTo(filePos, //position within the file at which the transfer is to begin
+                                         (fileLength - filePos), //maximum number of bytes to be transferred
+                                         outChan); //destination channel
 
    } catch (Exception e1) {
       e1.printStackTrace();
    } finally {
-      System.out.println("Files used");
+      System.out.println("\nFiles used");
       System.out.printf("Input: %s%nOutput: %s%n", inputfile, outputfile);
-      System.out.println("All done now.");
+      System.out.printf("Actual bytes transferred: %d%n", bytestransferred);
+
+      System.out.println("\nAll done now");
    }
 
     // ---------------------------------------------------------------
